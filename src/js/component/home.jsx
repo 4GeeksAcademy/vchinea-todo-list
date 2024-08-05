5197893366ccb197d2c03ce99e97446ec0f229c2
Victor Chinea <77308527+nobystanders@users.noreply.github.com>
2024-08-05 22:34:18 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,61 @@
-import React from "react";
+import React, { useState } from 'react';
+import './TodoList.css';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask.trim()]);
+      setNewTask('');
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      addTask();
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="todo-list">
+      <h1>Todo List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={newTask}
+          onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
+          placeholder="Enter a new task"
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      {tasks.length === 0 ? (
+        <p className="no-tasks">No tasks, add a task</p>
+      ) : (
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index} className="task-item">
+              {task}
+              <span className="delete-icon" onClick={() => deleteTask(index)}>
+                &#x2715;
+              </span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
-export default Home;
+export default TodoList;
